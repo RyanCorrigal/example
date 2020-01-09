efee038bb77ca7e0dcae8f1acd0f972aa695be9d
Ryan Corrigal <ryan.corrigal@aircanada.ca>
2020-01-09 13:54:50 -0700

    Fixed modules to use TypeORM

--- src/hashtag/hashtag.module.ts
+++ src/hashtag/hashtag.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { HashtagService } from './hashtag.service';
 import { HashtagController } from './hashtag.controller';
+import { Hashtag } from '../entities/hashtag.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Hashtag])],
   providers: [HashtagService],
   controllers: [HashtagController]
 })
